b8f5e42f58d42355477adf6e805a9d8365e57e9a
ChungNA <anhchungvtn@gmail.com>
2021-10-31 21:03:31 +0700

    add project

---
+++ Web API/JWTAuth/IdentityAuth/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JWTAuth.IdentityAuth
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ Web API/JWTAuth/Models/RegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JWTAuth.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage ="UserName is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "EmailAddress is required")]
+        public string EmailAddress { get; set; }
+    }
+}
